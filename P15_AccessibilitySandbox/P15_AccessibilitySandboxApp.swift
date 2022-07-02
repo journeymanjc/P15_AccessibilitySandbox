ae87d4096d3e0905395f9489767980c5c72a4ff0
Jae Cho <mobilewiz@gmail.com>
2022-07-02 00:04:20 -0400

    Initial Commit

---
+++ P15_AccessibilitySandbox/P15_AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  P15_AccessibilitySandboxApp.swift
+//  P15_AccessibilitySandbox
+//
+//  Created by Jae Cho on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct P15_AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
