4cf22f5b8ccfc960eda706947277a91e9d44a274
Jae Cho <mobilewiz@gmail.com>
2022-07-04 00:23:15 -0400

    Day 74-3

--- P15_AccessibilitySandbox/ContentView.swift
+++ P15_AccessibilitySandbox/ContentView.swift
@@ -21,29 +21,31 @@ struct ContentView: View {
 		"Sunflowers",
 		"Fireworks"
 	]
-	@State private var selectedPicture = Int.random(in: 0...3)
+	@State private var value = 10
 	
 	var body: some View {
-		Image(pictures[selectedPicture])
-			.resizable()
-			.scaledToFit()
-			.onTapGesture {
-				selectedPicture = Int.random(in: 0...3)
-			}
-			.accessibilityLabel(labels[selectedPicture])
-			.accessibilityAddTraits(.isButton)
-			.accessibilityRemoveTraits(.isImage)
-		
-		Image(decorative: "character")
-			.accessibilityHidden(true)
-		
 		VStack{
-			Text("Your score is")
-			Text("1000")
-				.font(.title)
+			Text("Value: \(value)")
+			Button("Increment"){
+				value += 1
+			}
+			Button("Decrement"){
+				value -= 1
+			}
 		}
-		.accessibilityElement(children: .ignore)
-		.accessibilityLabel("Your score is 1000")
+		.accessibilityElement()
+		.accessibilityLabel("Value")
+		.accessibilityValue(String(value))
+		.accessibilityAdjustableAction { direction in
+			switch direction {
+			case .increment:
+				value += 1
+			case .decrement:
+				value -= 1
+			default:
+				print("Not handled.")
+			}
+		}
     }
 }
 
